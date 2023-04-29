e99a540cf0bc77dcf7edc2ef7596e3912ec57ad4
sloppybook <sloppybook@gmail.com>
2023-04-29 18:42:49 +0900

    add kiki

---
+++ components/Kiki/index.jsx
@@ -0,0 +1,18 @@
+import styles from  './styles.module.css'
+
+export function Kiki() {
+  return (
+    <>
+      <div className={styles.moon}></div>
+      <div className={`${styles.kiki} ${styles.bounce}`}></div>
+      <div className={styles.star}></div>
+      <div className={styles.star}></div>
+      <div className={styles.star}></div>
+      <div className={styles.star}></div>
+      <div className={styles.small_star}></div>
+      <div className={styles.small_star}></div>
+      <div className={styles.small_star}></div>
+      <div className={styles.small_star}></div>
+    </>
+  )
+}
---
+++ components/index.js
@@ -0,0 +1,1 @@
+export { Kiki } from './Kiki/index.jsx'
